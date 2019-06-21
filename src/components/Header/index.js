58facb519ca378161cf2fcb55abf061c132b22f7
Juan <jnvleite@gmail.com>
2019-06-21 20:51:29 -0300

    done

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
 import {
   Container, Top, Logo, Title,
 } from './styles';
-
-import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import logo from '~/assets/Nubank_Logo.png';
 
--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,11 +1,5 @@
 import Reactotron from 'reactotron-react-native';
 
-if (__DEV__) {
-  const tron = Reactotron.configure()
-    .useReactNative()
-    .connect();
-
-  tron.clear();
-
-  console.tron = tron;
-}
+Reactotron.configure()
+  .useReactNative()
+  .connect();
